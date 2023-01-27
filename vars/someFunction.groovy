9ce23c1c2e206501e5c50ca7648fc5ba44f6bdc8
Nicolaj 'figaw' Græsholt <figaw@hotmail.com>
2023-01-27 09:56:06 +0100

    CLEANUP example

--- vars/someFunction.groovy
+++ vars/someFunction.groovy
@@ -1,9 +1,4 @@
 def call(input){
     echo "sent ${input}"
-    thenThis("other file");
     thenThis.thenThisOtherFunction("subfunction");
-}
-
-def someOtherFunction(input) {
-    echo "someOtherFunction ${input}"
 }
